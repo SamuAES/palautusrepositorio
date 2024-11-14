97ba5bc4a4ece4cdcbac3ff505ccf2e9a7d57a11
win <samuli.siukonen@gmail.com>
2024-11-14 19:04:36 +0200

    delete images and test test script

--- viikko3/web-login/src/testausta.py
+++
@@ -1,5 +0,0 @@
-from repositories.user_repository import user_repository
-from services.user_service import user_service
-
-result = user_service.validate("asd", "asdasdasd11", "asdasdasd1")
-print(result)
